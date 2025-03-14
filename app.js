9a1bf92090dc6bdfefa5b418fd1a7183c34d8b4c
Ecobz Thai <75235629+ManonWorkingArea@users.noreply.github.com>
2025-03-15 00:05:35 +0700

    transcode

--- app.js
+++ app.js
@@ -62,7 +62,8 @@ const storageSchema = new mongoose.Schema({
   createdAt: { type: Date, default: Date.now }, // วันที่สร้าง
   updatedAt: { type: Date, default: Date.now }, // วันที่อัปเดต
   duration: { type: Number, default: 0 }, // ระยะเวลา (สำหรับไฟล์มีเดีย)
-  thumbnail: { type: String, default: '' } // URL ของ thumbnail
+  thumbnail: { type: String, default: '' }, // URL ของ thumbnail
+  transcode: { type: Object, default: {} } // เพิ่มฟิลด์ transcode
 });
 
 // สร้างโมเดล Storage
