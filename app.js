55a8fd678fb9534363d5d40eab766ec502b3d0a5
Ecobz Thai <75235629+ManonWorkingArea@users.noreply.github.com>
2025-03-14 23:28:19 +0700

    S3

--- app.js
+++ app.js
@@ -253,18 +253,27 @@ async function processQueue(taskId, taskData) {
 
   await Task.updateOne({ taskId }, { status: 'processing' }); // อัปเดตสถานะใน MongoDB
   console.log(taskData.space);
-
+  
   let s3Key = taskData.space.s3Key;
   let s3Secret = taskData.space.s3Secret;
 
   // ตั้งค่า S3 โดยใช้ข้อมูลจาก taskData
-
+  const s3Client = new S3({
+    endpoint: `${taskData.space.s3EndpointDefault}`, // Include bucket in the endpoint
+    region: `${taskData.space.s3Region}`, // DigitalOcean Spaces does not require a specific region
+    ResponseContentEncoding:"utf-8",
+    credentials: {
+      accessKeyId: s3Key, // Ensure they are valid strings
+      secretAccessKey: s3Secret
+    },
+    forcePathStyle: false // DigitalOcean Spaces does NOT use path-style addressing
+  });
 
   console.log('S3 Client Config:', {
     endpoint: taskData.space.s3EndpointDefault,
     region: taskData.space.s3Region,
-    accessKeyId: s3Key,
-    secretAccessKey: s3Secret,
+    accessKeyId: taskData.space.s3Key,
+    secretAccessKey: taskData.space.s3Secret,
     bucket: taskData.space.s3Bucket
   });
 
